e4cf27eedc4b39bd0d84eedecbcfcd368b10c4aa
Derek Meneghel <derekmeneghel2004@gmail.com>
2025-09-29 00:02:38 -0300

    Commit

---
+++ src/main/java/com/example/Derek/ProjectVanzzeti/ProjectVanzzetiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Derek.ProjectVanzzeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVanzzetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVanzzetiApplication.class, args);
+	}
+
+}
